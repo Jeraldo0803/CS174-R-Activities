ccd469b3cce561766af05cc987caf127255dfeeb
jerzowwnn <104250310+jerzowwnn@users.noreply.github.com>
2024-03-06 23:32:59 +0800

    general outline of data preprocessing and EDA

--- FA1.2/CS174-R-FA1.2.R
+++ FA1.2/CS174-R-FA1.2.R
@@ -10,17 +10,29 @@ bank_data<- read.csv("bank-full.csv", sep=";")
 head(bank_data)
 str(bank_data)
 
-bank_data$y <- ifelse(bank_data$y == "no", 0, 1)
+#bank_data$y <- ifelse(bank_data$y == "no", 0, 1)
 
-model <- lm(bank_data$y ~ bank_data$balance + bank_data$duration, data=bank_data)
+#model <- lm(bank_data$y ~ bank_data$balance + bank_data$duration, data=bank_data)
 
-summary(model)
+#summary(model)
 
-plot(model)
+#plot(model)
 
 ##### DATA PREPROCESSING #####
+#Check for null values
+
+#Convert categorical to numerical
+
+#Remove Outliers
 
 ##### EXPLORATORY DATA ANALYSIS #####
+#Generate Correlation Matrix of Original Features
+
+#Calculate Feature Importance
+
+#Remove Unimportant Attributes
+
+#Check Correlation of Important Features
 
 ##### MODELLING #####
 

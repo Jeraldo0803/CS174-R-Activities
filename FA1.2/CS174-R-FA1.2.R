005741fc22fc430661bf8c6d91c5413d2dcc1ea4
jerzowwnn <104250310+jerzowwnn@users.noreply.github.com>
2024-03-07 14:37:20 +0800

    Code for converting categorical to num, corr matrix,  added png of corr matrrix

--- FA1.2/CS174-R-FA1.2.R
+++ FA1.2/CS174-R-FA1.2.R
@@ -1,7 +1,6 @@
 ##### DATA LOADING #####
 #import necessary libraries
 library(readr)
-library(dplyr)
 
 #get working directory
 getwd()
@@ -10,7 +9,7 @@ getwd()
 bank_data <- read.csv("bank-full.csv", sep=";")
 head(bank_data)
 str(bank_data)
-shape(bank_data)
+dim(bank_data)
 
 ##### DATA PREPROCESSING #####
 #Check for null values per column
@@ -18,13 +17,36 @@ null_values <- colSums(is.na(bank_data))
 print(null_values)
 
 #Convert categorical to numerical
-one_hot_vars <- dummy_cols(bank_data, columns= "job", "marital", "contact", "poutcome")
+#One-hot encoding
+cols_to_convert <- c("job", "marital", "contact", "poutcome")
+one_hot_data <- model.matrix(~ . + 0, data= bank_data[cols_to_convert])
+print(one_hot_data)
+bank_data <- bank_data[, !(names(bank_data) %in% c("job", "marital", "contact", "poutcome"))]
+encoded_bank_data <- cbind(bank_data, one_hot_data)
 
-#Remove Outliers
+#Label Encoding
+encoded_bank_data$education_num <- as.numeric(factor(encoded_bank_data$education))
+encoded_bank_data$default_num <- as.numeric(factor(encoded_bank_data$default))
+encoded_bank_data$housing_num <- as.numeric(factor(encoded_bank_data$housing))
+encoded_bank_data$loan_num <- as.numeric(factor(encoded_bank_data$loan))
+encoded_bank_data$month_num <- as.numeric(factor(encoded_bank_data$month))
+encoded_bank_data$y_num <- as.numeric(factor(encoded_bank_data$y))
+converted_bank_data <- encoded_bank_data[, !(names(encoded_bank_data) %in% c("education", "default", "housing", "loan", "month", "y"))]
+
+#Final Converted Data
+str(converted_bank_data)
+head(converted_bank_data)
 
 ##### EXPLORATORY DATA ANALYSIS #####
-
 #Generate Correlation Matrix of Original Features
+corr_matrix <- cor(converted_bank_data)
+#Visualize correlation matrix
+# Visualize the correlation matrix as a heatmap
+heatmap(corr_matrix,
+        symm = TRUE,         # Ensure the heatmap is symmetric
+        margins = c(10,10),  # Adjust margins for labels
+        main = "Correlation Matrix",  # Title of the plot
+        col = colorRampPalette(c("blue", "white", "red"))(100))  # Color scheme
 
 #Calculate Feature Importance
 
@@ -35,6 +57,17 @@ one_hot_vars <- dummy_cols(bank_data, columns= "job", "marital", "contact", "pou
 #Count instances of Relevant Features
 #Ex:job_count <- table(bank_data$job)
 #Ex:print(job_count)
+
+#Remove Outliers
+#Create boxplot
+#Age
+#boxplot(converted_bank_data$age, main = "Boxplot of Age Data", ylab = "Age", col = "yellow")
+
+#Balance
+#boxplot(converted_bank_data$balance, main = "Boxplot of Balance Data", ylab = "Balance", col = "yellow")
+
+#Duration
+#boxplot(converted_bank_data$duration, main = "Boxplot of Duration Data", ylab = "Duration", col = "yellow")
 
 ##### MODELLING #####
 
